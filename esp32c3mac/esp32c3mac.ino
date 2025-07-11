61ed6190d602587804cea97914ece0e146d1c769
tawinan <tawinan.ch@gmail.com>
2025-07-11 16:14:04 +0700

    fix: initialize WiFi before getting MAC address

--- esp32c3mac/esp32c3mac.ino
+++ esp32c3mac/esp32c3mac.ino
@@ -3,6 +3,13 @@
 void setup(){
   Serial.begin(115200);
   Serial.println();
+
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  Serial.println("Setup done");
   Serial.print("ESP32 Board MAC Address:  ");
   Serial.println(WiFi.macAddress());
 }
